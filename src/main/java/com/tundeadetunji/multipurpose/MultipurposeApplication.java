0b303cbe6bf1de5c775fb949c962297eeac1ab47
tundeadetunji <tundeadetunji2017@gmail.com>
2022-06-03 12:51:38 +0100

    Fresh from Spring Initializr

---
+++ src/main/java/com/tundeadetunji/multipurpose/MultipurposeApplication.java
@@ -0,0 +1,13 @@
+package com.tundeadetunji.multipurpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipurposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipurposeApplication.class, args);
+	}
+
+}
